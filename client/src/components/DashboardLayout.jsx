993dc4ada157bd6d23ef144f52665df432578e51
Pratik Kuril <pratikkuril16@gmail.com>
2025-12-24 19:07:23 +0530

    Remove unused active item

--- client/src/components/DashboardLayout.jsx
+++ client/src/components/DashboardLayout.jsx
@@ -1,10 +1,10 @@
 import { SidebarProvider, SidebarInset } from "@/components/ui/sidebar";
 import { AppSidebar } from "@/components/AppSidebar";
 
-export function DashboardLayout({ children, activeItem }) {
+export function DashboardLayout({ children }) {
   return (
     <SidebarProvider>
-      <AppSidebar activeItem={activeItem} />
+      <AppSidebar />
       <SidebarInset className="bg-[#faf9f6]">{children}</SidebarInset>
     </SidebarProvider>
   );
