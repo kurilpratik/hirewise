20aa26338068be3951e8cc7a8c2bb13bb6dbf06f
Pratik Kuril <pratikkuril16@gmail.com>
2025-12-24 05:55:33 +0530

    Setup navigation using react-router

--- client/src/layouts/DashboardLayout.jsx
+++ client/src/layouts/DashboardLayout.jsx
@@ -1,14 +1,15 @@
 import React from "react";
 import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
 import { AppSidebar } from "@/components/AppSidebar";
+import { Outlet } from "react-router-dom";
 
-const DashboardLayout = ({ children }) => {
+const DashboardLayout = () => {
   return (
     <SidebarProvider>
       <AppSidebar />
       <main className="h-screen w-full rounded-4xl border border-white bg-white">
         <SidebarTrigger className="block sm:hidden" />
-        {children}
+        <Outlet />
       </main>
     </SidebarProvider>
   );
