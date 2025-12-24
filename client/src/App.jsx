26e5688a9a1b9b5c887abe3f94cdf6ff3e72ecdf
Pratik Kuril <pratikkuril16@gmail.com>
2025-12-24 06:03:39 +0530

    Remove app.jsx since serving no purpose rn

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,4 @@
 import "./App.css";
-import DashboardLayout from "@/layouts/DashboardLayout";
 
 function App() {
   return <></>;
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,7 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import App from "./App.jsx";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import LandingPage from "./pages/LandingPage";
 import DashboardLayout from "./layouts/DashboardLayout";
@@ -24,6 +23,6 @@ const router = createBrowserRouter([
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <RouterProvider router={router} />
-    <App />
+    {/* <App /> */}
   </StrictMode>,
 );
