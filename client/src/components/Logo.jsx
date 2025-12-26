fb1b5b37bbd4c46297cbd2c61f59284aede67ea6
Pratik Kuril <pratikkuril16@gmail.com>
2025-12-26 20:18:29 +0530

    Update logo to lead to the landing

--- client/src/components/Logo.jsx
+++ client/src/components/Logo.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import { cn } from "@/lib/utils";
+import { Link } from "react-router-dom";
 
 const Logo = ({ className, ...props }) => {
   return (
     <h3 className={cn("font-faustina text-xl font-bold", className)} {...props}>
-      Hire<span className="text-primary">Wise</span>
+      <Link to={"/"}>
+        Hire<span className="text-primary">Wise</span>
+      </Link>
     </h3>
   );
 };
